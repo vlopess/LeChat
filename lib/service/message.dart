4d0b57a8d2c4babe1ddea792c7b0a481fc6a04c9
victor <victorlopes9291@gmail.com>
2024-02-04 12:36:34 -0300

    #2 - Criação dos métodos para verificar usuário e deleteChat

--- lib/service/message.dart
+++ lib/service/message.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
 import 'dart:math';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
+import 'package:lechat/repositories/firebase_storage_repository.dart';
 import 'package:lechat/service/auth.dart';
+import 'package:lechat/utils/encrypt.dart';
+import 'package:lechat/utils/message_enum.dart';
 
 
 
@@ -24,18 +28,38 @@ class ChatService {
 
   ChatService({required this.auth, required this.firestore, required this.ref});
 
-  void sendMessage(String? text, String chatId){
+  void sendMessage(String? text, String chatId, MessageEnum message){
     if(text!.isNotEmpty){
       var date = DateTime.now();
-      var message = {'userName': ref.read(authentication).getCurrentUser()!.displayName,'message': text, 'date' : date, 'photoURL' : ref.read(authentication).getCurrentUser()!.photoURL};            
-      firestore.collection('connections').doc(chatId).collection('messages').doc().set(message);
-      firestore.collection('connections').doc(chatId).update({'lastMessage' : text, 'date' : DateFormat('kk:mm').format(date)});
+      var messageData = {'userName': ref.read(authentication).getCurrentUser()!.displayName,'message': Encrypt.encrypt(text), 'date' : date, 'photoURL' : ref.read(authentication).getCurrentUser()!.photoURL, 'type' : message.description};            
+      firestore.collection('connections').doc(chatId).collection('messages').doc().set(messageData);
+      String msg;
+      switch (message) {
+        case MessageEnum.image:
+          msg = '📷​ Image';
+          break;
+        case MessageEnum.audio:
+          msg = '🎙️ Audio';
+          break;
+        case MessageEnum.gif:
+          msg = '🖼️​ GIF'; 
+          break;
+        case MessageEnum.video:
+          msg = '🎬​ Video';
+          break;
+        case MessageEnum.text:
+          msg = text;
+          break;
+      }
+      firestore.collection('connections').doc(chatId).update({'lastMessage' : Encrypt.encrypt(msg), 'date' : DateFormat('kk:mm').format(date)});
     }
   }
 
-  // Stream<QuerySnapshot>? getAllMessages(){
-  //   return firestore.collection('message').orderBy("date", descending: false).snapshots();
-  // }
+  void sendImageGIF(File file, String chatId, MessageEnum message) async{
+    var user = ref.read(authentication).getCurrentUser();
+    String imageURL = await ref.read(firebaseStorageRepository).storeFileToFirebase('connection/${message.description}/${user!.uid}/${file.path}', file);
+    sendMessage(imageURL, chatId, message);
+  }
 
   Future<String> createChat(String roomName) async {
     var user = ref.read(authentication).getCurrentUser()!;
@@ -49,7 +73,7 @@ class ChatService {
   Future<Map<String, String?>> existsCodeOrUser(String codeChat) async{
     var user = ref.read(authentication).getCurrentUser()!;
     var chat = await firestore.collection('connections').where('code',isEqualTo: codeChat).get();    
-    if (chat.docs.isEmpty) return { 'value' : 'There is no connection to this code'};    
+    if (chat.docs.isEmpty) return { 'value' : 'Connection not found'};    
     var data = await firestore.collection('connections').doc(chat.docs.first.id).get();
     userList = data['users'];
     if (userList!.where((element) => element == user.uid).isNotEmpty ) return { 'value' : "You're already in that connection"};    
@@ -62,6 +86,13 @@ class ChatService {
     await firestore.collection('connections').doc(chatId).update(newdata);
     var doc = await firestore.collection('connections').doc(chatId).get();
     userList!.clear();
+    return doc; 
+  }
+
+  Future<DocumentSnapshot<Map<String, dynamic>>> updateNameConnection(String chatId, String newName) async {
+    var newdata = {'roomName' : newName};
+    await firestore.collection('connections').doc(chatId).update(newdata);
+    var doc = await firestore.collection('connections').doc(chatId).get();
     return doc; 
   }
 
@@ -79,13 +110,42 @@ class ChatService {
     }
   }
 
+  Stream<QuerySnapshot<Map<String, dynamic>>> verificarUser(String chatId) {
+    var user = ref.read(authentication).getCurrentUser()!;
+    return firestore.collection('connections')    
+    .where('users', arrayContains: user.uid)
+    .where('chatId', isEqualTo: chatId)
+    .snapshots();
+  }
+
+  Future<void> removerUser(String chatId, String userId) async {
+    try {
+      var data = await firestore.collection('connections').doc(chatId).get();
+      userList = data['users'];
+      userList!.remove(userId);
+      var newdata = {'users' : userList};
+      await firestore.collection('connections').doc(chatId).update(newdata);
+      userList!.clear(); 
+    } catch (e) {
+      throw "Não foi possível remover";
+    }
+  }
+
+  Future<void> deleteChat(String chatId) async {
+    try {
+      await firestore.collection('connections').doc(chatId).delete();
+    } catch (e) {
+      throw "Não foi possível sair";
+    }
+  }
+
   Stream<QuerySnapshot<Map<String, dynamic>>> getAllChats() {
     var user = ref.read(authentication).getCurrentUser()!;
     var teste =  firestore.collection('connections').where('users', arrayContains: user.uid.toString()).snapshots();    
     return teste;
   }
 
-  Stream<QuerySnapshot<Map<String, dynamic>>> getAllMessagesChat(String chatId){        
+  Stream<QuerySnapshot<Map<String, dynamic>>> getAllMessagesChat(String chatId){ 
     return firestore.collection('connections').doc(chatId).collection('messages').orderBy("date", descending: false).snapshots();
   }
   
