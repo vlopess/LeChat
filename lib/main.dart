e84e0a40152aad9975a2438009832fcd019121a5
victor <victorlopes9291@gmail.com>
2024-02-13 22:49:52 -0300

    #5 - Iniciação as notificações no main

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lechat/screens/cadastre_screen.dart';
 import 'package:lechat/screens/home_screen.dart';
 import 'package:lechat/service/auth.dart';
+import 'package:lechat/service/firebaseapi.dart';
 import 'package:lechat/utils/theme_data.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
@@ -11,11 +12,15 @@ import 'package:lechat/screens/login_screen.dart';
 import 'package:lechat/screens/registration_screen.dart';
 import 'package:lechat/screens/welcome_screen.dart';
 
+
+final navigatorKey = GlobalKey<NavigatorState>();
+
 void main() async { 
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await FirebaseApi().initNotifications();
   runApp(const ProviderScope(child: LeChat()));
 }
 
@@ -34,6 +39,7 @@ class _LeChatState extends ConsumerState<LeChat> {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.theme,
       darkTheme: AppTheme.darktheme,
+      navigatorKey: navigatorKey,
       initialRoute: ref.read(authentication).getCurrentUser() == null ? LoginScreen.id : HomeScreen.id,
       routes: {
         WelcomeScreen.id :(context) => const WelcomeScreen(),
@@ -41,7 +47,7 @@ class _LeChatState extends ConsumerState<LeChat> {
         RegistrationScreen.id :(context) => const RegistrationScreen(),
         CadastreScreen.id :(context) => CadastreScreen(),
         HomeScreen.id :(context) => const HomeScreen(),
-        ChatScreen.id :(context) => const ChatScreen(), 
+        ChatScreen.id :(context) => ChatScreen(),  
       },
     );
   }
