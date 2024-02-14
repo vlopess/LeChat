60fcb037a6442d109cf7f5f5d999f97f37fcc8b9
victor <victorlopes9291@gmail.com>
2024-02-13 22:56:03 -0300

    #5 - Adicionando o token na model de Usuario

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -3,15 +3,17 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Usuario {
   String? name;
   String? uid;
+  String? token;
   String? profilePic;
 
-  Usuario({required this.name, required this.uid, required this.profilePic});    
+  Usuario({required this.name, required this.uid, required this.profilePic, required this.token});    
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['name'] = name;
     data['uid'] = uid;
     data['profilePic']  = profilePic;
+    data['token']  = token;
     return data;
   }
 
@@ -19,12 +21,14 @@ class Usuario {
     name = json['name'];
     uid = json['uid'];
     profilePic = json['profilePic'];
+    token = json['token'];
   }
 
   Usuario.fromObject(QueryDocumentSnapshot<Object?>  obj) {
     name = obj['name'];
     uid = obj['uid'];
     profilePic = obj['profilePic'];
+    token = obj['token'];
   }
 
   
